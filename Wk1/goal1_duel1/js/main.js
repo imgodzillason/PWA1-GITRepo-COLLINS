b1f2090ad2b034f439006f63881640c8895137d6
imgodzillason <jwcollins@fullsail.edu>
2015-01-09 14:16:23 -0500

    Testing fight
    Testing fight and added alert for each round

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -39,14 +39,40 @@
           var f1 = Math.floor(Math.random() * (playerOneDamage - minDamage1) + minDamage1);
           var f2 = Math.floor(Math.random() * (playerTwoDamage - minDamage2) + minDamage2);
 
-          console.log(f1);
-          console.log(f2);
+          //infllict damage
+          playerOneHealth -= f1;
+          playerTwoHealth -= f2;
+
+            console.log(playerOneName+":"+playerOneHealth+ " " +playerTwoName+":"+playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if (results ==="no winner"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER* " +playerTwoName+":"+playerTwoHealth);
+            }else{
+                alert(results);
+                break;
+            }
         };
     };
 
     //check for winner
     function winnerCheck(){
-        //code will go here
+        console.log("in winner check");
+
+        var result = "no winner";
+
+        if(playerOneHealth<1 && playerTwoHealth<1) {
+            result = "You both die.";
+        }else if(playerOneHealth < 1){
+            result = playerTwoName + " WINS!!!";
+        }else if(playerTwoHealth < 1){
+            result = playerOneName + " WINS!!!";
+        };
+
+        return result;
 
     };
 
