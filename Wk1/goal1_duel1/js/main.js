a42e8f7ba63b36d963be4e2040e30e12988f3e8d
imgodzillason <jwcollins@fullsail.edu>
2015-01-08 22:08:03 -0500

    Followed along with video for week 1 assignment
    Followed along with video for week 1 assignment

---
+++ Wk1/goal1_duel1/js/main.js
@@ -0,0 +1,45 @@
+/**
+ Jacob Collins
+ PWA- Section 1
+ 1/10/15
+ Analyze Duel #1
+ */
+
+//self-executing function
+
+(function(){
+
+    console.log("FIGHT!");
+
+    //player name
+    var playerOneName = "Wolverine";
+    var playerTwoName = "Deadpool";
+
+    //player damage
+    var playerOneDamage = 20;
+    var playerTwoDamage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+    //round
+    var round = 1;
+
+    //fight function
+    function fight(){
+        console.log("in the fight function");
+        winnerCheck()
+    };
+
+    //check for winner
+    function winnerCheck(){
+        //code will go here
+
+    };
+
+    //program begins
+    console.log("program start");
+    fight();
+
+})();
