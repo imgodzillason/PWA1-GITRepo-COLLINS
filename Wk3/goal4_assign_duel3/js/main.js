958591daa9ee1bc32f46e8b7da9ec1e57d73ca60
imgodzillason <jwcollins@fullsail.edu>
2015-01-22 19:35:30 -0500

    continuing to test duel 3
    continuing to test duel 3, currently experiencing issues with css
    formatting and “no winner” showing up in the health area as well as in
    the fight area.

--- Wk3/goal4_assign_duel3/js/main.js
+++ Wk3/goal4_assign_duel3/js/main.js
@@ -7,91 +7,69 @@
 
 //self-executing function
 
-(function(){ //start of function
+(function() { //start of function
 
     console.log("FIGHT!"); //print fight to console
 
-/* //edit out previous variables to replace with array
-    //player's name
-    var playerOneName = "Wolverine"; //player one name
-    var playerTwoName = "Deadpool"; //player two name
-
-    //player's damage
-    var playerOneDamage = 20; //player one damage
-    var playerTwoDamage = 20; //player two damage
-
-    //player's health
-    var playerOneHealth = 100; //player one health
-    var playerTwoHealth = 100; //player two health
-*/
-    //array for player stats
-
-    //var playerStats1 = ["Wolverine", 20, 100];  //array for player 1 stats (name, damage, health)
-
-    //var playerStats2 = ["Deadpool", 20, 100];  //array for player 2 stats (name, damage, health)
-
-    //fighter information
+    //create object with three properties for both fighters in array
     var players = [
-        playerOne = {name: "Deadpool", damage: 20, health: 100}
-        playerTwo = {name: "Wolverine", damage: 20, health: 100}
+        playerOne = {name: "Deadpool", damage: 20, health: 100}, //first fighter object properties
+        playerTwo = {name: "Wolverine", damage: 20, health: 100} //second fighter object properties
     ];
 
-
-
     //round starting point
-    var round = 1;
+    var round = 0;
+
+    //create variable for fight button
+    var button = document.querySelector(".buttonblue");
+
+    //player health start
+    document.getElementById("kabal").innerHTML = players[0].health; //show Deadpool's health
+    document.getElementById("kratos").innerhtml = players[1].health; //show Wolverine's health
 
     //fight function
-    function fight(){
-        console.log("in the fight function"); //test to make sure function called correctly
+    function fight() {
 
-        //player health start
-        document.getElementById("kabal").innerHTML = players[0].health; //show Deadpool's health
-        document.getElementById("kratos").innerhtml = players[1].health; //show Wolverine's health
+        var minDamage1 = players[0].damage * .5; //minimum damage halved for player 1 edit: changed variable to array for player 1
+        var minDamage2 = players[1].damage * .5; //minimum damage halved for player 2 edit: changed variable to array for player 2
+        //round down integer after multiplying random number by the max damage subtracted by min damage, after added to min damage for player 1
+        var f1 = Math.floor(Math.random() * (players[0].damage - minDamage1) + minDamage1); //edit:changed variable to match new array for player 1
+        //round down integer after multiplying random number by the max damage subtracted by min damage, after added to min damage for player 2
+        var f2 = Math.floor(Math.random() * (players[1].damage - minDamage2) + minDamage2);// edit: changed variable to match new array for player 2
 
+        //inflict damage
+        players[0].health -= f1; //damage inflicted on player one //edit: changed array to new object set for player 1
+        players[1].health -= f2; //damage inflicted on player two //edit: changed array to new object set for player 2
 
+        var results = winnerCheck(); //check for winner by calling function
 
-        //alert(playerStats1[0]+":"+playerStats1[2]+"  *START*  "+playerStats2[0]+":"+playerStats2[1]); //pop up showing name, health, and start //edit:changed variables to match arrays
+        if (results === "no winner") { //if no winner, increase next round and pop up name, health, and round of both players
+            round++;
 
-        for(var i=0; i<10; i++){    //set up number of times to go through function...start at 0 and go until it reaches 10 by adding 1 every time
-          //Math.floor(Math.random() * (max - min) + min); //formula to determine damage
+            document.getElementById("kabal").innerHTML = players[0].health; //show Deadpool's health
+            document.getElementById("kratos").innerHTML = players[1].health; //show Wolverine's health
+            document.getElementById("round_number").innerHTML = "ROUND " + round + ": COMPLETED!";
 
-          var minDamage1 = players[0].damage * .5; //minimum damage halved for player 1 edit: changed variable to array for player 1
-          var minDamage2 = players[1].damage * .5; //minimum damage halved for player 2 edit: changed variable to array for player 2
-            //round down integer after multiplying random number by the max damage subtracted by min damage, after added to min damage for player 1
-          var f1 = Math.floor(Math.random() * (players[0].damage - minDamage1) + minDamage1); //edit:changed variable to match new array for player 1
-            //round down integer after multiplying random number by the max damage subtracted by min damage, after added to min damage for player 2
-          var f2 = Math.floor(Math.random() * (players[1].damage - minDamage2) + minDamage2);// edit: changed variable to match new array for player 2
+        } else {
+            document.getElementById("scores").innerHTML = endResults;
+            document.getElementById("round_number").innerHTML = endResults;
 
-          //inflict damage
-          players[0].health -= f1; //damage inflicted on player one //edit: changed array to new object set for player 1
-          players[1].health -= f2; //damage inflicted on player two //edit: changed array to new object set for player 2
-            //print both player's names and health to console
-            console.log(playerStats1[0]+":"+playerStats1[2]+ " " +playerStats2[0]+":"+playerStats2[2]); //edit: changed output to match new array
+            button.innerHTML = "FINISH HIM!";
+            button.setAttribute("onclick", null);
+        }; //close out else statement
+    }; //close out for loop
 
-            var results = winnerCheck(); //check for winner by calling function
-            console.log(results); //print out results to console
-
-            if (results ==="no winner"){ //if no winner, increase next round and pop up name, health, and round of both players
-                round++;
-                alert(players[0].name+":"+players[0].health+"  *ROUND "+round+" OVER* " +players[1].name+":"+players[1].health); //edit: changed output to match new array
-            }else{
-                alert(results); //pop up results
-                break; //stop code if meets requirements
-            } //close out else statement
-        } //close out for loop
-    } //close out fight function
 
     //check for winner
-    function winnerCheck(){ //check for winner
+    function winnerCheck() { //check for winner
 
         var result = "no winner"; //if there isn't a winner, return no winner result
 
-        if(players[0].health<1 && players[1].health<1) { //if both player's health is below 1 //edit: changed variables to match new array for both players
+        if (players[0].health < 1 && players[1].health < 1) { //if both player's health is below 1 //edit: changed variables to match new array for both players
             result = "You both die."; //show that they both died
-        }else if(players[0].health < 1){ //if player one's health is less than 1 //edit: changed variables to match new array for both players
+        } else if (players[0].health < 1) { //if player one's health is less than 1 //edit: changed variables to match new array for both players
             result = players[1].name + " WINS!!!"; //show player two wins //edit: changed variables to match new array for both players
-        }else if(players[1].health < 1){ //if player two's health is less than 1 //edit: changed variables to match new array for both players
+        } else if (players[1].health < 1) { //if player two's health is less than 1 //edit: changed variables to match new array for both players
             result = players[0].name + " WINS!!!"; //show player one wins //edit: changed variables to match new array for both players
         } //close out else/if statement
 
@@ -99,8 +77,14 @@
 
     } //close out function winnerCheck
 
-    //program begins
-    console.log("program start");
-    fight(); //begin program
+    var endResults = winnerCheck();
 
-})(); //self-invoking to start program
+    //program begins
+
+    button.onclick = function (e) {
+
+        fight();
+        e.preventDefault();
+        return false;
+    };
+})();
