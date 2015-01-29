9206397943eed2412dc7f078e3ad3ef2261f2f73
imgodzillason <jwcollins@fullsail.edu>
2015-01-29 09:54:14 -0500

    continue creating functions and parameters
    continue creating functions and parameters, set up constructor

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -9,6 +9,7 @@
 
     var names = ["Mr. Blonde", "Mr. Brown", "Mr. White", "Mr. Pink", "Mr. Blue"]; //names array
     var people = []; //people array created
+    var rowNumber = 1;
     var rowOne = getElementById("r1c1"); //set up variable for row 1
     var rowTwo = getElementById("r2c1"); //set up variable for row 2
     var rowThree = getElementById("r3c1"); //set up variable for row 3
@@ -16,10 +17,18 @@
     for(var i=0; i<3; i++){ //for loop initiated and continues until it reaches 3
         var nameIndex = Math.floor(Math.random() * names.length); //creates random number to choose name from array
 
+        var person = new Person(names[nameIndex], rowNumber); // creates new person and places in row
+        var personName = person.name; //creates variable personName and sets it to equal person.name
 
-    }
+        people.push(person); //push person name to array
+        if(personName != rowOne.innerHTML && personName != rowTwo.innerHTML && personName != rowThree.innerHTML){ //set if statement to get names
 
 
+
+
+        }
+
+        function populateHTML()
 
 
 
--- Wk4/goal7_assign_objects/js/person.js
+++ Wk4/goal7_assign_objects/js/person.js
@@ -7,6 +7,12 @@
 
 window.person = Person; //storing person for use in main.js file
 
-Person.jobs = ["Driver", "Gunner", "Runner", "Mastermind"];
-Person.actions = ["Sleeping", "Committing Heist", "Driving", "Talking"];
+Person.jobs = ["Driver", "Gunner", "Runner", "Mastermind"]; //set up jobs in array
+Person.actions = ["Sleeping", "Committing Heist", "Driving", "Talking"]; //set up actions in array
 
+function Person (names, row){ //create constructor function using names and row as parameters/keys
+    this.name = names; //assign value stored in names parameter
+    this.actions = person.actions[Math.floor(Math.random() * Person.length.actions)]; //assign single value for action choice
+    this.row = row; //assign value to row passed in parameter
+
+}
