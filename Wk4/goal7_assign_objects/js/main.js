dde4bc9fae456953a12127d70cf7482eda8ab4ff
imgodzillason <jwcollins@fullsail.edu>
2015-01-28 22:54:54 -0500

    setting up variables
    setting up variables and began for loop for week 4 assignment

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -1,6 +1,36 @@
 /**
- * Created by the JavaScript Development Team
+ * Jacob Collins
  * Class: PWA
  * Goal: Goal7
+ * 1/30/15
  */
 
+(function(){ //create self-executing function
+
+    var names = ["Mr. Blonde", "Mr. Brown", "Mr. White", "Mr. Pink", "Mr. Blue"]; //names array
+    var people = []; //people array created
+    var rowOne = getElementById("r1c1"); //set up variable for row 1
+    var rowTwo = getElementById("r2c1"); //set up variable for row 2
+    var rowThree = getElementById("r3c1"); //set up variable for row 3
+
+    for(var i=0; i<3; i++){ //for loop initiated and continues until it reaches 3
+        var nameIndex = Math.floor(Math.random() * names.length); //creates random number to choose name from array
+        
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
